5da4f0d4964f1ea19f30c4f96cf24b974ea71f0d
Eli Skeggs <eli@mixmax.com>
2020-05-21 12:17:52 -0700

    chore: fix linting

--- test/fixtures/print-interpolate.js
+++ test/fixtures/print-interpolate.js
@@ -1,3 +1,4 @@
 import interpolate from './interpolate.hbs';
 
+// eslint-disable-next-line no-console
 console.log(interpolate({ name: 'Human' }).trim());
--- test/fixtures/print-simple.js
+++ test/fixtures/print-simple.js
@@ -1,3 +1,4 @@
 import simple from './simple.hbs';
 
+// eslint-disable-next-line no-console
 console.log(simple().trim());
